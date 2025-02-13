79cb69a4632fb65b4a0c50530a68be7b0126cc7e
Rianda <e41231605@student.polije.ac.id>
2025-02-13 18:37:38 +0700

    Tugas BKPM Acara 3-6

---
+++ BKPM_acara_3-4/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+use Illuminate\Support\Facades\Route;
+
+Route::get('/profile', function () {
+    return view('profile'); 
+});
+
+
+
+
---
+++ BKPM_acara_5-6/routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ManagementUserController;
+
+Route::get('user', [ManagementUserController::class, 'index']);
+Route::get('user/create', [ManagementUserController::class, 'create']);
+Route::post('user', [ManagementUserController::class, 'store']);
+Route::get('user/{id}', [ManagementUserController::class, 'show']);
+Route::get('user/{id}/edit', [ManagementUserController::class, 'edit']);
+Route::put('user/{id}', [ManagementUserController::class, 'update']);
+Route::delete('user/{id}', [ManagementUserController::class, 'destroy']);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get("/home", function(){
+    return view("home");
+});
+
