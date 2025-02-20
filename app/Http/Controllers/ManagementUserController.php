9b52f693dc3de4a3fe5a453886cee93f868e0f75
Rianda <e41231605@student.polije.ac.id>
2025-02-20 10:43:56 +0700

    Acara 5 Web Membuat konsep MVC

---
+++ app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManagementUserController
+{
+    public function index()
+    {
+        return "Halo ini adalah method index, dalam controller ManagementUser";
+    }
+}
