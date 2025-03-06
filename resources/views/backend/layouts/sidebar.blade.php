8aeb50c3f57614b060312158abf1e20ed76e85aa
Rianda <e41231605@student.polije.ac.id>
2025-03-06 09:28:02 +0700

    Mengupdate views sidebar Acara 13-16

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -5,21 +5,42 @@
 
         <!-- Dashboard -->
         <li class="nav-item">
-            <a class="nav-link {{ Request::routeIs('dashboard.index') ? '' : 'collapsed' }}"
-                href="{{ route('dashboard.index') }}">
+            <a class="nav-link {{ Request::routeIs('dashboard.backend') ? '' : 'collapsed' }}"
+                href="{{ route('dashboard.backend') }}">
                 <i class="bi bi-grid"></i>
                 <span>Dashboard</span>
             </a>
         </li>
 
-        <!-- Product -->
+        <!-- Riwayat Hidup -->
+        @php
+            $isRiwayatActive =
+                Request::routeIs('pendidikan.backend', 'pendidikan.backend.add', 'edit.pendidikan') ||
+                Request::routeIs('pengalamankerja.backend', 'pengalamankerja.backend.add', 'pengalamankerja.edit');
+        @endphp
         <li class="nav-item">
-            <a class="nav-link {{ Request::routeIs('product.index') ? '' : 'collapsed' }}"
-                href="{{ route('product.index') }}">
-                <i class="bi bi-box"></i>
-                <span>Product</span>
+            <a class="nav-link {{ $isRiwayatActive ? '' : 'collapsed' }}" data-bs-target="#tables-nav"
+                data-bs-toggle="collapse" href="#">
+                <i class="bi bi-layout-text-window-reverse"></i><span>Riwayat Hidup</span><i
+                    class="bi bi-chevron-down ms-auto"></i>
             </a>
-        </li>
+            <ul id="tables-nav" class="nav-content collapse {{ $isRiwayatActive ? 'show' : '' }}"
+                data-bs-parent="#sidebar-nav">
+                <li>
+                    <a href="{{ route('pendidikan.backend') }}"
+                        class="{{ Request::routeIs('pendidikan.backend', 'pendidikan.backend.add', 'edit.pendidikan') ? 'active' : '' }}">
+                        <i class="bi bi-circle"></i><span>Pendidikan</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('pengalamankerja.backend') }}"
+                        class="{{ Request::routeIs('pengalamankerja.backend', 'pengalamankerja.backend.add', 'pengalamankerja.edit') ? 'active' : '' }}">
+                        <i class="bi bi-circle"></i><span>Pengalaman Kerja</span>
+                    </a>
+                </li>
+            </ul>
+        </li><!-- End Tables Nav -->
+
     </ul>
 
 </aside><!-- End Sidebar-->
